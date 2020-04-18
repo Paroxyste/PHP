6735312e385dddab934b167213cdd0c133de9588
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-18 14:02:47 +0200

    Fix require() & main panel

--- Social-Network/view/close_account.php
+++ Social-Network/view/close_account.php
@@ -3,57 +3,81 @@
 declare(strict_types=1);
 
 require('./view/header.php');
-require('./controller/form_handlers/close_account_handler.php');
+require('./view/navbar.php');
+require('./view/user_counter.php');
 
-require('./view/dashboard/navbar.php');
-require('./view/dashboard/user_counter.php');
-require('./view/dashboard/left_panel.php');
+require('./controller/form_handlers/close_account_handler.php');
 
 ?>
 
-<div class="col-lg-8 col-xl-8">
-    <div class="card-box">
+<div class="row">
+    <div class="col-lg-4 col-xl-4">
 
-        <h5 class="mb-3 text-uppercase bg-light p-2">
-            <i class="ti-close mr-1"></i>
-            Close Account
-        </h5>
+        <?php
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="alert alert-danger text-center" role="alert">
+        if (
+            $userLoggedIn = $_SESSION['username']
+        ) {
+            require('./view/dashboard/user_details_upl.php');
+            require('./view/dashboard/message_box.php');
+        } else {
+            require('./view/dashboard/user_details.php');
+        }
 
-                        <strong>
-                            Are tou sure you want to close your account ?
-                        </strong>
+        ?>
 
-                        <p class="mt-3">
-                            Closing your account will hide your profile and
-                            all your activity from other users.
-                        </p>
+    </div>
 
-                        <p>
-                            You can re-open your account at any time by
-                            simply logging in.
-                        </p>
+    <div class="col-lg-8 col-xl-8">
+        <div class="card-box card-details">
+
+            <h5 class="mb-3 text-uppercase bg-light p-2">
+                <i class="ti-close mr-1"></i>
+                Close Account
+            </h5>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="alert alert-danger text-center" role="alert">
+
+                            <strong>
+                                Are tou sure you want to close your account ?
+                            </strong>
+
+                            <p class="mt-3">
+                                Closing your account will hide your profile and
+                                all your activity from other users.
+                            </p>
+
+                            <p>
+                                You can re-open your account at any time by
+                                simply logging in.
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="clearfix text-right">
-                    <form action="close_account.php" method="POST">
-                        <button class="btn btn-success" type="submit"
-                                name="cancel">
-                            Cancel
-                        </button>
+                    <div class="clearfix text-right">
+                        <form action="close_account.php" method="POST">
+                            <button class="btn btn-success" type="submit"
+                                    name="cancel">
+                                Cancel
+                            </button>
 
-                        <button class="btn btn-danger" type="submit"
-                                name="close_account">
-                            Yes, Close it !
-                        </button>
-                    </form>
+                            <button class="btn btn-danger" type="submit"
+                                    name="close_account">
+                                Yes, Close it !
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<?php
+
+require('./view/footer.php');
+
+?>
