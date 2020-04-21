d44f4ab032872d91d2cc6eb1812900c95500cc04
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-21 11:43:43 +0200

    Upd import

--- Social-Network/upload.php
+++ Social-Network/upload.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+include('./view/header.php');
+include('./view/navbar.php');
+include('./view/user_counter.php');
 require('./view/upload.php');
+include('./view/footer.php');
 
 ?>
