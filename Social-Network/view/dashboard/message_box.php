7f9979b971012ba7fc9c2b3bbf486141d74ccde1
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-24 12:04:47 +0200

    Upd space

--- Social-Network/view/dashboard/message_box.php
+++ Social-Network/view/dashboard/message_box.php
@@ -6,7 +6,6 @@ $msgObj = new Message($con, $userLoggedIn);
 
 ?>
 
-<!-- Start conversations card -->
 <div class="card-box card-details">
     <h4 class="header-title mb-3">
         Messages
@@ -18,42 +17,41 @@ $msgObj = new Message($con, $userLoggedIn);
          style="max-height: auto;">
 
         <?php
-            if (
-                $msgObj->GetConvers() == NULL
-            ) {
-                echo "
-                    <div class='alert alert-warning alert-dismissible 
-                                fade show' 
-                         role='alert'>
 
-                        <button type='button' 
-                                class='close' 
-                                data-dismiss='alert' 
-                                aria-label='Close'>
+        if (
+            $msgObj->GetConvers() == NULL
+        ) {
+            echo "
+                <div class='alert alert-warning alert-dismissible 
+                            fade show' 
+                     role='alert'>
 
-                            <span aria-hidden='true'>
-                                &times;
-                            </span>
-                        </button>
+                    <button type='button' 
+                            class='close' 
+                            data-dismiss='alert' 
+                            aria-label='Close'>
 
-                        Oh, you haven't start a conversation with your 
-                        friends.
-                    </div>
+                        <span aria-hidden='true'>
+                            &times;
+                        </span>
+                    </button>
 
-                    <div class='text-center'>
-                        <a class='btn btn-blue waves-effect waves-light 
-                                  text-white mt-2'
-                           href='messages.php?u=new'>
-                            Start a conversation
-                        </a>
-                    </div>
-                ";
-            } else {
-                echo $msgObj->GetConvers();
-            }
+                    Oh, you haven't start a conversation with your friends.
+                </div>
+
+                <div class='text-center'>
+                    <a class='btn btn-blue waves-effect waves-light 
+                              text-white mt-2'
+                       href='messages.php?u=new'>
+                        Start a conversation
+                    </a>
+                </div>
+            ";
+        } else {
+            echo $msgObj->GetConvers();
+        }
 
         ?>
 
     </div>
-    <!-- end inbox-widget -->
 </div>
