81c7a1c8c9ad5e090fd0e49f75c6b292424bc929
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-24 09:41:53 +0200

    Upd import

--- Social-Network/requests.php
+++ Social-Network/requests.php
@@ -1,5 +1,11 @@
 <?php
 
-include('./view/requests.php')
+declare(strict_types=1);
+
+include('./view/header.php');
+include('./view/navbar.php');
+include('./view/user_counter.php');
+include('./view/requests.php');
+include('./view/footer.php');
 
 ?>
