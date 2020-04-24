f16a01f335c2165fa8740773611a81bbd3cfbf19
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-24 17:35:37 +0200

    Add all requierment

--- Social-Network/close_account.php
+++ Social-Network/close_account.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+require('./view/header.php');
+require('./view/navbar.php');
+require('./view/user_counter.php');
 require('./view/close_account.php');
+require('./view/footer.php');
 
 ?>
