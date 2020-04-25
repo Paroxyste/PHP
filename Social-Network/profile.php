db105977511266ba48232b81d04b213ff1fc4ce6
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-25 11:18:50 +0200

    Del space

--- Social-Network/profile.php
+++ Social-Network/profile.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 require('./view/header.php');
-
 require('./view/navbar.php');
 require('./view/user_counter.php');
 require('./view/dashboard.php');
-
 require('./view/footer.php');
 
 ?>
