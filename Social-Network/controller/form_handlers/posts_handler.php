0880fd6cf2119e979bdd72cf3a43c5f19ecea331
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-26 17:46:46 +0200

    Add filter & Fix Error

--- Social-Network/controller/form_handlers/posts_handler.php
+++ Social-Network/controller/form_handlers/posts_handler.php
@@ -1,55 +1,55 @@
 <?php
 
-declare(strict_types=1);
-
 if (
     isset($_POST['post_btn'])
 ) {
-    $uploadOk = 1;
+    $uploadOk  = 1;
     $imageName = $_FILES['fileToUpload']['name'];
-    $uploadMsg = '';
+    $uploadMsg = NULL;
+
+    // ----------------------------------------------------------- Check upload
 
     if (
-        $imageName != ''
+        $imageName != NULL
     ) {
         $targetDir     = 'view/images/attached-files/';
         $imageName     = $targetDir . uniqid() . basename($imageName);
         $imageFileType = pathinfo($imageName, PATHINFO_EXTENSION);
 
+        // --------------------------------------------------- Error image size
+
         if (
             $_FILES['fileToUpload']['size'] > 3000000
         ) {
             $uploadMsg = "
-                <div class='col-md-8'>
-                    <div class='alert alert-danger alert-dismissible
-                                fade show mb--2'
-                         role='alert'>
+                <div class='alert alert-danger alert-dismissible
+                            fade show mb--2'
+                     role='alert'>
 
-                        <span class='alert-inner--icon'>
-                            <i class='fas fa-exclamation-triangle mr-2'></i>
+                    <span class='alert-inner--text'>
+                        <strong>
+                            Error :
+                        </strong>
+                        Your file is too large ! (max: 3MB)
+                    </span>
+
+                    <button type='button' 
+                            class='close'
+                            data-dismiss='alert' 
+                            aria-label='Close'>
+
+                        <span aria-hidden='true'>
+                            &times;
                         </span>
 
-                        <span class='alert-inner--text'>
-                            <strong>
-                                Error :
-                            </strong>
-                            Your file is too large ! (max: 3MB)
-                        </span>
-
-                        <button type='button' class='close'
-                                data-dismiss='alert' aria-label='Close'>
-
-                            <span aria-hidden='true'>
-                                &times;
-                            </span>
-
-                        </button>
-                    </div>
+                    </button>
                 </div>
             ";
 
             $uploadOk = 0;
         }
+
+        // ----------------------------------------------- Error picture format
 
         if (
             strtolower($imageFileType) != 'jpeg'
@@ -58,93 +58,141 @@ if (
             && strtolower($imageFileType) != 'gif'
         ) {
             $uploadMsg = "
-                <div class='col-md-8'>
-                    <div class='alert alert-danger alert-dismissible
-                                fade show mb--2'
-                         role='alert'>
+                <div class='alert alert-danger alert-dismissible
+                            fade show mb--2'
+                     role='alert'>
 
-                        <span class='alert-inner--icon'>
-                            <i class='fas fa-exclamation-triangle mr-2'></i>
+                    <span class='alert-inner--text'>
+                        <strong>
+                            Error :
+                        </strong>
+                        Only jpeg, jpg, png or gif files are allowed !
+                    </span>
+
+                    <button type='button' 
+                            class='close'
+                            data-dismiss='alert' 
+                            aria-label='Close'>
+
+                        <span aria-hidden='true'>
+                            &times;
                         </span>
 
-                        <span class='alert-inner--text'>
-                            <strong>
-                                Error :
-                            </strong>
-                            Only jpeg, jpg, png or gif files are allowed !
-                        </span>
-
-                        <button type='button' class='close'
-                                data-dismiss='alert' aria-label='Close'>
-
-                            <span aria-hidden='true'>
-                                &times;
-                            </span>
-
-                        </button>
-                    </div>
+                    </button>
                 </div>
             ";
 
             $uploadOk = 0;
         }
 
+        // ----------------------------------------------------- Success upload
+
         if (
-            $uploadOk
+            empty($uploadMsg)
         ) {
-            if (
-                move_uploaded_file($_FILES['fileToUpload']['tmp_name'],
-                                   $imageName)
-            ) {
-                $uploadMsg = "
-                    <div class='col-md-8'>
-                        <div class='alert alert-success alert-dismissible
-                                    fade show mb--2'
-                             role='alert'>
-
-                            <span class='alert-inner--icon'>
-                                <i class='fas fa-check mr-2'></i>
-                            </span>
-
-                            <span class='alert-inner--text'>
-                                <strong>
-                                    Succes :
-                                </strong>
-                                Image uploaded successfully !
-                            </span>
-
-                            <button type='button' class='close'
-                                    data-dismiss='alert' aria-label='Close'>
-
-                                <span aria-hidden='true'>
-                                    &times;
-                                </span>
-
-                            </button>
-                        </div>
-                    </div>
-                ";
-            } else {
-                $uploadOk = 0;
-            }
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $imageName);
         }
     }
 
+    // ------------------------------------------------------------- Check post
+
+    $userNone = 'none';
+    $postedTo = strip_tags($_POST['posted_to']);
+    $postedBy = strip_tags($_POST['posted_by']);
+
+    // -------------------------------------------------------- Filter postText
+
+    $postText = filter_data(
+                    filter_var($_POST['post_text'], FILTER_SANITIZE_STRING)
+                );
+
+    // ------------------------------------------------------------ Empty field
     if (
-        $uploadOk
+        empty($postText)
     ) {
-        $post = new POST($con, $_POST['posted_by']);
+        $uploadMsg = "
+            <div class='alert alert-danger alert-dismissible
+                        fade show mb--2'
+                 role='alert'>
+
+                <span class='alert-inner--text'>
+                    <strong>
+                        Error :
+                    </strong>
+                    This field is required !
+                </span>
+
+                <button type='button' 
+                        class='close'
+                        data-dismiss='alert' 
+                        aria-label='Close'>
+
+                    <span aria-hidden='true'>
+                        &times;
+                    </span>
+
+                </button>
+            </div>
+        ";
+
+        $uploadOk = 0;
+    }
+
+    // ------------------------------------------------------------ Limit filed
+
+    if (
+        strlen($postText) < 1
+        || strlen($postText) > 160
+    ) {
+        $uploadMsg = "
+            <div class='alert alert-danger alert-dismissible
+                        fade show mb--2'
+                 role='alert'>
+
+                <span class='alert-inner--text'>
+                    <strong>
+                        Error :
+                    </strong>
+                    Your comment must be between 1 and 160 characters !
+                </span>
+
+                <button type='button' 
+                        class='close'
+                        data-dismiss='alert' 
+                        aria-label='Close'>
+
+                    <span aria-hidden='true'>
+                        &times;
+                    </span>
+
+                </button>
+            </div>
+        ";
+
+        $uploadOk = 0;
+    }
+
+    // ------------------------------------------------------------ Error check
+
+    if (
+        empty($uploadMsg)
+        && $uploadOk
+    ) {
+        $post = new POST($con, strip_tags($postedBy));
+
+        $postText  = $con->real_escape_string($postText);
+        $postedTo  = $con->real_escape_string($postedTo);
+        $userNone  = $con->real_escape_string($userNone);
+        $imageName = $con->real_escape_string($imageName);
+
+        // ---------------------------------------------------------- Add to DB
 
         if (
-            $_POST['posted_to'] == $_POST['posted_by']
+            $postedTo == $postedBy
         ) {
-            // If user is on own profile, user = 'none'
-            $post->SubmitPost($_POST['post_text'], 'none', $imageName);
+            $post->SubmitPost($postText, $userNone, $imageName);
         } else {
-            // Else user = '$_POST['posted_to']'
-            $post->SubmitPost($_POST['post_text'],
-                              $_POST['posted_to'],
-                              $imageName);
+            $post->SubmitPost($postText, $postedTo, $imageName);
         }
 
         echo "
@@ -154,7 +202,17 @@ if (
         ";
     }
 } else {
-    $uploadMsg = '';
+    $uploadMsg = NULL;
+}
+
+// ---------------------------------------------------------------- Data filter
+
+function filter_data($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlentities($data);
+
+    return $data;
 }
 
 ?>
