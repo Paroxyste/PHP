2c5d529339edf3fa1703df2314148c938bcfbf25
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-26 12:58:15 +0200

    Add filter and cond

--- Social-Network/controller/form_handlers/messages_handler.php
+++ Social-Network/controller/form_handlers/messages_handler.php
@@ -1,8 +1,11 @@
 <?php
 
-declare(strict_types=1);
+$msgObj = new Message($con, strip_tags($userLoggedIn));
 
-$msgObj = new Message($con, $userLoggedIn);
+$errMsg  = NULL;
+$userNew = 'new';
+
+// ---------------------------------------------------------------- New convers
 
 if (
     isset($_GET['u'])
@@ -14,31 +17,64 @@ if (
     if (
         $userTo == FALSE
     ) {
-      $userTo = 'new';
+        $userTo = $userNew;
     }
 }
 
+// --------------------------------------------------------------- Load Convers
+
 if (
-    $userTo != 'new'
+    $userTo != $userNew
 ) {
-    $userToObj = new User($con, $userTo);
+    $userToObj = new User($con, strip_tags($userTo));
 }
+
+// --------------------------------------------------------------- Post new msg
 
 if (
     isset($_POST['post_msg'])
 ) {
+    // Filter message
+    $message = filter_data(
+                    filter_var($_POST['msg_body'], FILTER_SANITIZE_STRING)
+                );
+
+    // Empty field
     if (
-        isset($_POST['msg_body'])
+        empty($message)
     ) {
-        $message = $_POST['msg_body'];
+        $errMsg = 'This field is required';
+    }
+
+    // Width filed
+    if (
+        strlen($message) < 1
+        || strlen($message) > 160
+    ) {
+        $errMsg = 'Your message must be between 1 and 160 characters';
+    }
+
+    if (
+        empty($errMsg)
+    ) {
         $dateTime = date('Y-m-d H:i:s');
 
         $message  = $con->real_escape_string($message);
         $dateTime = $con->real_escape_string($dateTime);
         $userTo   = $con->real_escape_string($userTo);
-
+    
         $msgObj->SendMessage($userTo, $message, $dateTime);
     }
+}
+
+// ---------------------------------------------------------------- Data filter
+
+function filter_data($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlentities($data);
+
+    return $data;
 }
 
 ?>
