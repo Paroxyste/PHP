85119e147af2388239c64307f5807ca1d292928a
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-29 14:16:31 +0200

    Add folder redirect

--- Social-Network/config/config.php
+++ Social-Network/config/config.php
@@ -4,21 +4,69 @@ declare(strict_types=1);
 
 session_start();
 
+// error_reporting(0);
+
 $timezone = date_default_timezone_set('Europe/Paris');
 
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db   = 'my_bdd';
+$db   = 'social';
 
 $con = new mysqli($host, $user, $pass, $db);
 
-// check connection
+// ----------------------------------------------------------- Check connection
 if (
     mysqli_connect_errno()
 ) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
+}
+
+// ---------------------------------------------------------------- Redirection
+
+// Config folder
+if (
+    $_SERVER['REQUEST_URI'] == '/config/'
+) {
+    echo "
+        <script>
+            location.href='../index.php'
+        </script>
+    ";
+}
+
+// Controller folder
+if (
+    $_SERVER['REQUEST_URI'] == '/controller/'
+) {
+    echo "
+        <script>
+            location.href='../index.php'
+        </script>
+    ";
+}
+
+// Model folder
+if (
+    $_SERVER['REQUEST_URI'] == '/model/'
+) {
+    echo "
+        <script>
+            location.href='../../index.php'
+        </script>
+    ";
+}
+
+// View folder
+if (
+    $_SERVER['REQUEST_URI'] == '/view/'
+) {
+    echo "
+        <script>
+            location.href='../index.php'
+        </script>
+    ";
 }
 
 ?>
