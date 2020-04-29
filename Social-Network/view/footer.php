730b70b9a4bd224814257fbdf0c5253adf6f0551
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-29 19:34:52 +0200

    Upd footer style

--- Social-Network/view/footer.php
+++ Social-Network/view/footer.php
@@ -1,28 +1,21 @@
     <footer class="footer">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-6">
-            &copy; 2019 - My Social Network theme by
-            <a href="#">Laurent-Dev.fr</a>
-          </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 text-uppercase text-footer">
+                    &copy; 2019 - My Social Network by
+                    <a href="https://laurent-dev.fr">Laurent-Dev</a>
+                </div>
 
-          <div class="col-md-6">
-            <div class="text-md-right footer-links d-none d-sm-block">
-              <a href="javascript:void(0);">
-                About Us
-              </a>
-
-              <a href="javascript:void(0);">
-                Help
-              </a>
-
-              <a href="javascript:void(0);">
-                Contact Us
-              </a>
+                <div class="col-md-6">
+                    <div class="text-md-right footer-links text-footer
+                                d-none d-sm-block text-uppercase">
+                        <a href="https://laurent-dev.fr">
+                            Retour au site
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </footer>
 
     <!-- Load script -->
