26dc9487ad35f0930e0d29578d4c6404e2b9d8c9
Laurent Echeverria <45815049+Paroxyste@users.noreply.github.com>
2020-04-29 20:03:50 +0200

    Add breakout

---
+++ Social-Network/breakout.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+include('./view/header.php');
+include('./view/navbar.php');
+include('./view/user_counter.php');
+include('./view/games/breakout.php');
+include('./view/footer.php');
+
+?>
